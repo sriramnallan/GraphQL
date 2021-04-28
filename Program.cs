2376d1e780a37f95985375748f80162007dc921d
sriram nallan <sriramnallan@gmail.com>
2021-04-28 11:58:55 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Types;
+using GraphQL.SystemTextJson;
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var schema = Schema.For(@"
+      type Query {
+        hello: String
+      }
+    ");
+
+        var json = await schema.ExecuteAsync(_ =>
+        {
+            _.Query = "{ hello }";
+            _.Root = new { Hello = "Hello World!" };
+        });
+
+        Console.WriteLine(json);
+    }
+}
